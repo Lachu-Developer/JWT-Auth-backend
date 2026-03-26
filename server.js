f2ef3a63280daf4f328adf2644e0397967242992
Lachu Leo <lakshmanan.unq@gmail.com>
2026-03-26 16:06:32 +0530

    fix port for render

--- server.js
+++ server.js
@@ -21,8 +21,10 @@ app.get("/", (req, res) => {
   res.send("Backend is running 🚀")
 })
 
-app.listen(3001, () => {
-  console.log("Server running on port 3001 🚀")
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`)
 })
 
 console.log("Server file running")
